89ad666528a53307e9d337fe3f5386c61d1bc853
Niraj Tiwari <tniraj7@gmail.com>
2021-12-29 23:19:06 +0900

    add `SearchController` to tableview for search functionality, display dummy data in tableview

--- GithubApp/GithubApp/HomeViewController.swift
+++ GithubApp/GithubApp/HomeViewController.swift
@@ -1,11 +1,57 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    let array : [Int] = (1...100).map { _ in  Int.random(in: 1...99) }
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.obscuresBackgroundDuringPresentation = false
+        return searchController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         title = "Github Search"
         navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.searchController = searchController
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension HomeViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            return UITableViewCell()
+        }
+        cell.backgroundView?.backgroundColor = .systemTeal
+        cell.textLabel?.text = "\(array[indexPath.row])"
+        return cell
     }
 }
 
