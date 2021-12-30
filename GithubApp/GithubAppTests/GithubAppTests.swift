661411b105905ca61de8680b229f6cbe6ea1c110
Niraj Tiwari <tniraj7@gmail.com>
2021-12-31 02:45:04 +0900

    add first test and make it pass

--- GithubApp/GithubAppTests/GithubAppTests.swift
+++ GithubApp/GithubAppTests/GithubAppTests.swift
@@ -1,6 +1,22 @@
 import XCTest
 @testable import GithubApp
 
+class GithubServiceSpy: GithubService {
+    private(set) var loadRepositoriesAPICallCount: Int = 0
+    func loadGithubRepositoryData(keyword: String, completion: @escaping ((Result<GithubAPIResponse, Error>) -> Void)) {
+        loadRepositoriesAPICallCount += 1
+    }
+}
+
 class GithubAppTests: XCTestCase {
 
+    func test_viewDidLoad_doesntLoadGithubRepositoryData() {
+        
+        let service = GithubServiceSpy()
+        let sut = SearcViewController(service: service)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(service.loadRepositoriesAPICallCount, 0)
+    }
 }
--- GithubApp/GithubAppUITests/GithubAppUITests.swift
+++ GithubApp/GithubAppUITests/GithubAppUITests.swift
@@ -1,4 +1,4 @@
-import XCTest
+import XCTest
 
 class GithubAppUITests: XCTestCase {
 }
