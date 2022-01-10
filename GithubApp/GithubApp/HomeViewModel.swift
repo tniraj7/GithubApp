f76435267b8fa4b4cbacfdb3ee3fc9eb131d3218
Niraj Tiwari <tniraj7@gmail.com>
2022-01-11 01:10:54 +0900

    retry, send empty when error, send empty when no keyword

--- GithubApp/GithubApp/HomeViewModel.swift
+++ GithubApp/GithubApp/HomeViewModel.swift
@@ -10,24 +10,26 @@ class HomeViewModel {
         return searchString.asObservable()
             .debounce(.milliseconds(500), scheduler: MainScheduler.instance)
             .distinctUntilChanged()
-            .filter {( $0.allSatisfy({ $0.isLetter }) && $0.count>0 )}
+            .map { $0.lowercased() }
+            .filter { $0.allSatisfy({ $0.isLetter }) }
             .flatMap {
                 self.makeRequest($0)
-                    .map { response in
-                        return response.items ?? [Item]()
-                    }
+                    .map { $0.items ?? [] }
             }
-
     }
     
     private func makeRequest(_ keyword: String) -> Observable<GithubAPIResponse> {
         let finalUrl = URL(string: "https://api.github.com/search/repositories?q=\(keyword)")!
         let request = URLRequest(url: finalUrl)
         
-        return URLSession.shared.rx.data(request: request)
-            .map { data in
-                try JSONDecoder().decode(GithubAPIResponse.self, from: data)
+        return URLSession.shared.rx.response(request: request)
+            .subscribe(on: ConcurrentDispatchQueueScheduler(queue: DispatchQueue.global()))
+            .map { (_, data: Data) in
+                return try JSONDecoder().decode(GithubAPIResponse.self, from: data)
             }
+            .retry(2)
+            .catchAndReturn(.init(items: []))
             .observe(on: MainScheduler.asyncInstance)
+            .share(replay: 1, scope: .whileConnected)
     }
 }
